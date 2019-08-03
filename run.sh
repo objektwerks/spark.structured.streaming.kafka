55cc9d2b37b963e768ba1c63871e754f30912618
objektwerks <objektwerks@runbox.com>
2019-08-03 17:41:38 -0400

    edited run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 spark-submit \
-  --class aoi.AreaOfInterestApp \
+  --class streaming.KafkaStructuredStreamingApp \
   --master local[2] \
   --packages org.apache.spark:spark-sql-kafka-0-10:2.4.3,com.typesafe:config:1.3.4 \
   ./target/scala-2.12/kafka-spark-structured-streaming_2.12-0.1.jar
