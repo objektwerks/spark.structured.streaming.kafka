5506246554e39c346e2ebca2e856699248534b1d
objektwerks <objektwerks@runbox.com>
2019-08-05 06:59:44 -0400

    corrected package name in submit.sh

--- submit.sh
+++ submit.sh
@@ -2,5 +2,5 @@
 spark-submit \
   --class streaming.KafkaStructuredStreamingApp \
   --master local[2] \
-  --packages org.apache.spark:spark-sql-kafka-0-10:2.4.3,com.typesafe:config:1.3.4 \
+  --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.3,com.typesafe:config:1.3.4 \
   ./target/scala-2.11/kafka-spark-structured-streaming_2.11-0.1.jar
